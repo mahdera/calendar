95d7c17481f40147791a457f98401def0d55bbfc
Mahder Neway <mahdera@yahoo.com>
2014-12-11 07:44:37 -0500

    modified the database table

--- databasescript/db_script.sql
+++ databasescript/db_script.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 10, 2014 at 09:00 PM
+-- Generation Time: Dec 11, 2014 at 01:43 PM
 -- Server version: 5.5.38
 -- PHP Version: 5.6.2
 
@@ -27,20 +27,19 @@ create database db_event_calendar;
     `title` varchar(255) NOT NULL,
     `body` text NOT NULL,
     `start_time` varchar(100) NOT NULL,
-    `end_time` varchar(100) NOT NULL
-  ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+    `end_time` varchar(100) NOT NULL,
+    `modified_by` int(11) NOT NULL,
+    `modification_date` datetime NOT NULL
+  ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
   --
   -- Dumping data for table `tbl_event_calendar`
   --
 
-  INSERT INTO `tbl_event_calendar` (`id`, `title`, `body`, `start_time`, `end_time`) VALUES
-  (1, 'f', 's', '2014-12-10T13:00:00+01:00', '2014-12-10T13:30:00+01:00'),
-  (2, 'h', 'n', '2014-12-10T13:59:00+01:00', '2014-12-10T14:29:00+01:00'),
-  (3, 'd', 'p', '2014-12-08T02:00:00+01:00', '2014-12-08T02:30:00+01:00'),
-  (4, 'morngin', 'eight am', '2014-12-10T14:00:00+01:00', '2014-12-10T14:30:00+01:00'),
-  (5, 'God', 'plz', '2014-12-10T09:00:00+01:00', '2014-12-10T09:30:00+01:00'),
-  (6, 'Yes', 'Got it working', '2014-12-10T08:00:00+01:00', '2014-12-10T08:30:00+01:00');
+  INSERT INTO `tbl_event_calendar` (`id`, `title`, `body`, `start_time`, `end_time`, `modified_by`, `modification_date`) VALUES
+  (8, 'Interview', 'There is interview today at the specified time.', '2014-12-11T08:00:00+01:00', '2014-12-11T08:30:00+01:00', 0, '0000-00-00 00:00:00'),
+  (9, 'This is the last event', 'This is the body of the text that I want to play with', '2014-12-11T15:15:00+01:00', '2014-12-11T15:45:00+01:00', 0, '0000-00-00 00:00:00'),
+  (10, 'Perfect', 'This is working very much and I am loving it', '2014-12-11T09:00:00+01:00', '2014-12-11T09:30:00+01:00', 0, '0000-00-00 00:00:00');
 
   --
   -- Indexes for dumped tables
@@ -50,7 +49,7 @@ create database db_event_calendar;
   -- Indexes for table `tbl_event_calendar`
   --
   ALTER TABLE `tbl_event_calendar`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `title` (`title`,`start_time`,`end_time`);
 
   --
   -- AUTO_INCREMENT for dumped tables
@@ -60,4 +59,4 @@ create database db_event_calendar;
   -- AUTO_INCREMENT for table `tbl_event_calendar`
   --
   ALTER TABLE `tbl_event_calendar`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
